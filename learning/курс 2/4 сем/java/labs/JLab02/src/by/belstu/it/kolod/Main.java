9fec1e790ada33c4400469779c8311c4cbcd7aea
tepduck <lksunaylkolodl@gmail.com>
2021-02-15 08:13:09 +0300

    main function

---
+++ learning/курс 2/4 сем/java/labs/JLab02/src/by/belstu/it/kolod/Main.java
@@ -0,0 +1,11 @@
+package  by.belstu.it.kolod;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //TODO: add new  method
+
+        testFunc obj = new testFunc();
+    }
+}
