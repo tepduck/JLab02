5973f3dce7a9c44ef70bd79a0836a6303cf75089
tepduck <lksunaylkolodl@gmail.com>
2021-02-15 08:44:31 +0300

    main function: import class + testFunc

--- learning/курс 2/4 сем/java/labs/JLab02/src/by/belstu/it/kolod/Main.java
+++ learning/курс 2/4 сем/java/labs/JLab02/src/by/belstu/it/kolod/Main.java
@@ -1,5 +1,7 @@
 package  by.belstu.it.kolod;
 
+import by.belstu.it.kolod.testFunc;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +9,6 @@ public class Main {
         //TODO: add new  method
 
         testFunc obj = new testFunc();
+        obj.getValue();
     }
 }
---
+++ learning/курс 2/4 сем/java/labs/JLab02/src/by/belstu/it/kolod/testFunc.java
@@ -0,0 +1,13 @@
+package by.belstu.it.kolod;
+
+public class testFunc {
+
+    public String getValue()
+    {
+        sayHello();
+        return "Hello";}
+
+    public static void sayHello() {
+        System.out.printf("Hello from testFunc");
+    }
+}
